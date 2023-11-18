1c290e1298af74ef9d4f06c025735a8bcd96a9d1
Jeffrey Wang <ngjaywa@gmail.com>
2023-11-18 09:31:48 -0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # colab-langserve
+
+Make sure GPU is selected.
+
+## Setup
+
+```shell
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip install -r requirements.txt
+```
+
+Save the url from logger
---
+++ main.py
@@ -0,0 +1,57 @@
+from huggingface_hub import hf_hub_download
+from fastapi import FastAPI
+import json
+import uvicorn
+from pyngrok import ngrok
+from fastapi.middleware.cors import CORSMiddleware
+import nest_asyncio
+from pydantic import BaseModel
+from langchain.llms import LlamaCpp
+import logging
+from typing import String
+from langserve import add_routes
+from langchain_experimental.chat_models import Llama2Chat
+from langchain.llms import LlamaCpp
+
+
+logger = logging.getLogger(__name__)
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+repo = "TheBloke/samantha-mistral-7B-GGUF"
+model = "samantha-mistral-7b.Q4_0.gguf"
+
+path = hf_hub_download(repo_id=repo, filename=model)
+llama = LlamaCpp(model_path=path, n_gpu_layers=40, n_batch=512, streaming=True)
+
+
+add_routes(app, Llama2Chat(llm=llama), path="/chat")
+# class ChatRequest(BaseModel):
+#     prompt: String
+
+# @app.post('/chat')
+# def chat(req: ChatRequest):
+#     logger.info(req)
+#     llama()
+#     return llama.chat(req.text, req.context)
+
+
+def main():
+    ngrok_tunnel = ngrok.connect(8000)
+    logger.info('Public URL:', ngrok_tunnel.public_url)
+    nest_asyncio.apply()
+    uvicorn.run(app, port=8000)
+
+
+if __name__ == '__main__':
+
+    main()
