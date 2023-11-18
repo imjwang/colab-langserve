e774f6811c5f467c4c2dacc977534b624749690c
Jeffrey Wang <ngjaywa@gmail.com>
2023-11-18 13:22:36 -0800

    asdf

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-repo = "TheBloke/Llama-2-13B-chat-GGUF"
-model = "llama-2-13b-chat.Q6_K.gguf"
+repo = "TheBloke/Llama-2-7B-Chat-GGUF"
+model = "llama-2-7b-chat.Q2_K.gguf"
 
 path = hf_hub_download(repo_id=repo, filename=model)
 llama = LlamaCpp(model_path=path, n_gpu_layers=43, n_batch=512, stream=False)
